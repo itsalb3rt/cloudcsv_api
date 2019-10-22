90caa5e8bb26ba3e7601ce27de0696da3decc6c5
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-22 19:56:24 -0400

    add users model

---
+++ src/models/users/UsersModel.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace App\models\users;
+
+
+use System\Model;
+
+class UsersModel extends Model
+{
+    public function create($user){
+        $this->db()
+            ->table('users')
+            ->insert($user);
+    }
+
+    public function update($id,$user){
+        $this->db()
+            ->table('users')
+            ->where('id_user','=',$id)
+            ->update($user);
+    }
+
+    public function getAll($fields,$filter,$oderBy,$orderDir,$offset,$limit){
+        return $this->db()
+            ->select($fields)
+            ->table('users')
+            ->where($filter)
+            ->orderBy($oderBy,$orderDir)
+            ->offset($offset)
+            ->limit($limit)
+            ->getAll();
+    }
+
+    public function getById($id){
+        return $this->db()
+            ->table('users')
+            ->where('id_user','=',$id)
+            ->get();
+    }
+
+    public function getByUserName($userName){
+        return $this->db()
+            ->table('users')
+            ->where('user_name','=',$userName)
+            ->get();
+    }
+
+    public function delete($id){
+        $this->db()
+            ->table('users')
+            ->where('id_user','=',$id)
+            ->delete();
+    }
+}
