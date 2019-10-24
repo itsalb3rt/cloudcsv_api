e63014f43e8f307a8aaa1a146b78c617d79a5ec0
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-24 16:26:31 -0400

    add notificationemail interface

--- src/controllers/NotificationsemailsController.php
+++ src/controllers/NotificationsemailsController.php
@@ -1,10 +1,68 @@
 <?php
 
 
-namespace App\controllers;
+use App\plugins\QueryStringPurifier;
+use App\plugins\SecureApi;
+use App\plugins\Util;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\models\emailsNotifications\EmailsNotificationsModel;
 
-
-class NotificationsemailsController
+class NotificationsemailsController extends Controller
 {
+    private $request;
+    private $response;
 
+    public function __construct()
+    {
+        new SecureApi();
+        $this->request = Request::createFromGlobals();
+        $this->response = new Response();
+        $this->response->headers->set('content-type', 'application/json');
+    }
+
+    public function notificationsemails($id = null)
+    {
+        $util = new Util();
+        $notifications = new EmailsNotificationsModel();
+        switch ($this->request->server->get('REQUEST_METHOD')) {
+            case 'GET':
+                if ($id === null) {
+                    $qString = new QueryStringPurifier();
+                    $this->response->setContent(json_encode($notifications->getAll($qString->getFields(),
+                        $qString->fieldsToFilter(),
+                        $qString->getOrderBy(),
+                        $qString->getSorting(),
+                        $qString->getOffset(),
+                        $qString->getLimit())));
+                } else {
+                    $notifications = $notifications->getById($id);
+                    $this->response->setContent(json_encode($notifications));
+                }
+
+                $this->response->setStatusCode(200);
+                $this->response->send();
+                break;
+            case 'POST':
+                $newNotification = json_decode(file_get_contents('php://input'), true);
+                $util->validateEmail($newNotification['email']);
+                $notifications->create($newNotification);
+
+                $this->response->setContent('success');
+                $this->response->setStatusCode(201);
+                $this->response->send();
+                break;
+            case 'PATCH':
+                $this->response->setContent('Method Not Allowed');
+                $this->response->setStatusCode(405);
+                $this->response->send();
+                break;
+            case 'DELETE':
+                $notifications->delete($id);
+                $this->response->setContent('success');
+                $this->response->setStatusCode(200);
+                $this->response->send();
+                break;
+        }
+    }
 }
--- src/models/emailsNotifications/EmailsNotificationsModel.php
+++ src/models/emailsNotifications/EmailsNotificationsModel.php
@@ -4,7 +4,38 @@
 namespace App\models\emailsNotifications;
 
 
-class EmailsNotificationsModel
-{
+use System\Model;
 
+class EmailsNotificationsModel extends Model
+{
+    public function create($notification){
+        $this->db()
+            ->table('notifications_emails')
+            ->insert($notification);
+    }
+
+    public function getAll($fields,$filter,$oderBy,$orderDir,$offset,$limit){
+        return $this->db()
+            ->select($fields)
+            ->table('notifications_emails')
+            ->where($filter)
+            ->orderBy($oderBy,$orderDir)
+            ->offset($offset)
+            ->limit($limit)
+            ->getAll();
+    }
+
+    public function getById($id){
+        return $this->db()
+            ->table('notifications_emails')
+            ->where('id_notification_email','=',$id)
+            ->get();
+    }
+
+    public function delete($id){
+        $this->db()
+            ->table('notifications_emails')
+            ->where('id_notification_email','=',$id)
+            ->delete();
+    }
 }
