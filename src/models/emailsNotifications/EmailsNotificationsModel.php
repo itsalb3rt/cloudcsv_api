33a4b64fb1cd7c25023ba963f3184411e40947b9
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-24 16:24:53 -0400

    add rule for recibe Authorization token or bearer token

---
+++ src/models/emailsNotifications/EmailsNotificationsModel.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\models\emailsNotifications;
+
+
+class EmailsNotificationsModel
+{
+
+}
---
+++ src/models/tables/TablesModel.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\models\tables;
+
+
+class TablesModel
+{
+
+}
