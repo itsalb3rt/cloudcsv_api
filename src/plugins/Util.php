59fa52f441447602718d9c37606869d46a9b3c26
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-24 16:27:05 -0400

    Create a util class

--- src/plugins/Util.php
+++ src/plugins/Util.php
@@ -4,7 +4,21 @@
 namespace App\plugins;
 
 
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\RFCValidation;
+use Symfony\Component\HttpFoundation\Response;
+
 class Util
 {
-
+    public function validateEmail($email)
+    {
+        $validator = new EmailValidator();
+        if ($validator->isValid($email, new RFCValidation()) === false) {
+            $respose = new Response();
+            $respose->setContent('the email is not valid');
+            $respose->setStatusCode(409);
+            $respose->send();
+            die();
+        }
+    }
 }
