8e59fecd377ad850ace8d30a157063b5cff6b9bc
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-24 23:49:20 -0400

    add method to drop tables from data base

--- src/models/tables/TablesModel.php
+++ src/models/tables/TablesModel.php
@@ -8,13 +8,15 @@ use System\Model;
 
 class TablesModel extends Model
 {
-    public function create(string $table){
+    public function create(string $table)
+    {
         $this->db()
             ->query($table)
             ->exec();
     }
 
-    public function saveInDataStorage(array $table):int {
+    public function saveInDataStorage(array $table): int
+    {
         $this->db()
             ->table('table_storage')
             ->insert($table);
@@ -22,56 +24,68 @@ class TablesModel extends Model
         return $this->db()->insertId();
     }
 
-    public function saveColumn(array $column){
+    public function saveColumn(array $column)
+    {
         $this->db()
             ->table('tables_columns')
             ->insert($column);
     }
 
-    public function update(int $id,array $table):void{
+    public function update(int $id, array $table): void
+    {
         $this->db()
             ->table('table_storage')
-            ->where('id_table_storage','=',$id)
+            ->where('id_table_storage', '=', $id)
             ->update($table);
     }
 
-    public function getAll($fields,$filter,$oderBy,$orderDir,$offset,$limit){
+    public function getAll($fields, $filter, $oderBy, $orderDir, $offset, $limit)
+    {
         return $this->db()
             ->select($fields)
             ->table('table_storage')
             ->where($filter)
-            ->orderBy($oderBy,$orderDir)
+            ->orderBy($oderBy, $orderDir)
             ->offset($offset)
             ->limit($limit)
             ->getAll();
     }
 
-    public function getById($id){
+    public function getById($id)
+    {
         return $this->db()
             ->table('table_storage')
-            ->where('id_table_storage','=',$id)
+            ->where('id_table_storage', '=', $id)
             ->get();
     }
 
-    public function getByName($tableName){
+    public function getByName($tableName)
+    {
         return $this->db()
             ->table('table_storage')
-            ->where('table_name','=',$tableName)
+            ->where('table_name', '=', $tableName)
             ->get();
     }
 
-    public function getColumns($idTable){
+    public function getColumns($idTable)
+    {
         return $this->db()
             ->table('tables_columns')
-            ->where('id_table_storage','=',$idTable)
+            ->where('id_table_storage', '=', $idTable)
             ->getAll();
     }
 
-    public function delete($id):void{
+    public function delete($id): void
+    {
         $this->db()
             ->table('table_storage')
-            ->where('id_table_storage','=',$id)
+            ->where('id_table_storage', '=', $id)
             ->delete();
+    }
+
+    public function drop($name)
+    {
+        $this->db()->query("DROP TABLE IF EXISTS $name;")->exec();
     }
 
 }
