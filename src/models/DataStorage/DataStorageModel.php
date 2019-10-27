8de78979017f89cfd6adbc3fc814d140dae211a7
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-27 00:07:37 -0400

    add interface for processing data for dynamics tables

---
+++ src/models/DataStorage/DataStorageModel.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\models\DataStorage;
+
+
+use System\Model;
+
+class DataStorageModel extends Model
+{
+    public function create($table,$data){
+        $this->db()
+            ->table($table)
+            ->insert($data);
+    }
+
+    public function getAll($fields,$filter,$oderBy,$orderDir,$offset,$limit){
+        return $this->db()
+            ->select($fields)
+            ->table('notifications_emails')
+            ->where($filter)
+            ->orderBy($oderBy,$orderDir)
+            ->offset($offset)
+            ->limit($limit)
+            ->getAll();
+    }
+
+    public function getById($table,$id){
+        return $this->db()
+            ->table($table)
+            ->where('id_' . $table ,'=',$id)
+            ->get();
+    }
+
+    public function delete($table,$id){
+        $this->db()
+            ->table($table)
+            ->where('id_' . $table,'=',$id)
+            ->delete();
+    }
+}
