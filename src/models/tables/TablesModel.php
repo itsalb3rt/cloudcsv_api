b3125e5e728d85448f2ccbf682ccc622cf89a24a
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-10-29 23:27:13 -0400

    add method for processing columns modifications

--- src/models/tables/TablesModel.php
+++ src/models/tables/TablesModel.php
@@ -4,10 +4,19 @@
 namespace App\models\tables;
 
 
+use ConfigFileManager\ConfigFileManager;
 use System\Model;
 
 class TablesModel extends Model
 {
+    private $dbPrefix;
+
+    public function __construct()
+    {
+        $systemConfigIni = new ConfigFileManager(__ROOT__DIR__ . 'system/config/config.php.ini');
+        $this->dbPrefix = $systemConfigIni->prefix;
+    }
+
     public function create(string $table)
     {
         $this->db()
@@ -73,6 +82,39 @@ class TablesModel extends Model
             ->table('tables_columns')
             ->where('id_table_storage', '=', $idTable)
             ->getAll();
+    }
+
+    public function getColumnById($columnId){
+        return $this->db()
+            ->table('tables_columns')
+            ->where('id_table_colum', '=', $columnId)
+            ->get();
+    }
+
+    public function changeColumnName($tableName,$currentColumnName,$newColumnName){
+        $this->db()
+            ->query("ALTER TABLE $this->dbPrefix$tableName RENAME COLUMN $currentColumnName TO $newColumnName")
+            ->exec();
+    }
+
+    public function updateColumnNameOnTablecolumns($idColumn,$data){
+        $this->db()
+            ->table('tables_columns')
+            ->where('id_table_colum','=',$idColumn)
+            ->update($data);
+    }
+
+    public function updateColumnLength($idColumn,$data){
+        $this->db()
+            ->table('tables_columns')
+            ->where('id_table_colum','=',$idColumn)
+            ->update($data);
+    }
+
+    public function changeDataType($table,$column,$type){
+        $this->db()
+            ->query("ALTER TABLE $this->dbPrefix$table ALTER COLUMN $column TYPE $type USING $column::$type")
+            ->exec();
     }
 
     public function delete($id): void
