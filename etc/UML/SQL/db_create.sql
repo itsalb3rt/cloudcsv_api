42df75c0e8850359dfd9b81adc8ab26a462049f0
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-05 16:01:06 -0400

    public release

---
+++ etc/UML/SQL/db_create.sql
@@ -0,0 +1,9 @@
+-- Database: Untitled
+-- Author: 
+CREATE DATABASE untitled
+    WITH OWNER = postgres
+        ENCODING = 'UTF8'
+        TABLESPACE = pg_default
+        LC_COLLATE = 'undefined'
+        LC_CTYPE = 'undefined'
+        CONNECTION LIMIT = -1;
