564ab5d26b8b5cfd85457ba907d8895b5fa26c34
Albert Hidalgo <alhidalgodev@gmail.com>
2022-12-09 20:05:30 -0400

    feat
    * Set the database vars from dot env file

--- system/Model.php
+++ system/Model.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * Esta clase es el intermediario entre los modelos y
  * la clase Database, esto con el objetivo de mantener
  * una estructura solida y poco confusa
  **/
+
 namespace System;
 
 use Buki\Pdox;
@@ -14,22 +16,22 @@ class Model
     private $credentials;
     private $config;
 
-    public function db() {
-        if(is_null($this->bdd)) {
+    public function db()
+    {
+        if (is_null($this->bdd)) {
             // Leer credenciales desde el  archivo ini
             $this->credentials = parse_ini_file(__ROOT__DIR__ . "system/config/config.php.ini");
 
             $this->config = [
-                'host'		=> $this->credentials["host"],
-                'driver'	=> $this->credentials["driver"],
-                'database'	=> $this->credentials["dbname"],
-                'username'	=> $this->credentials["user"],
-                'password'	=> $this->credentials["pass"],
-                'charset'	=> $this->credentials["charset"],
-                'collation'	=> $this->credentials["collation"],
-                'prefix'	=> $this->credentials["prefix"],
-                'port'	=> $this->credentials["port"],
-                'debug' =>false
+                'host'        => $_ENV['POSTGRES_DATABASE_HOST'] . ":".$_ENV['DATABASE_PORT'],
+                'driver'    => "pgsql",
+                'database'    => $_ENV['POSTGRES_DATABASE_NAME'],
+                'username'    => $_ENV['POSTGRES_USER'],
+                'password'    => $_ENV['POSTGRES_PASSWORD'],
+                'charset'    => "utf8",
+                'collation'    => "utf8mb4_unicode_ci",
+                'prefix'    => "",
+                'port'    => $_ENV['DATABASE_PORT']
             ];
             /**
              * Pdox es un Query Builder usado para facilitar la manera en que se
